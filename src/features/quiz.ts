cb9644c332ab4671aa198cfdba3805ccb7e40d69
shlok1998 <shlokmehra@rocketmail.com>
2025-05-12 01:25:45 +0530

    feat: Redux Added

---
+++ src/features/quiz.ts
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+import Quiz from '../pages/Quiz';
+
+type QuizElement = {
+    question : string,
+    options: string[],
+    correctAnswer: string,
+    explanation: string
+}
+
+type Quiz = QuizElement[]
+
+type InitialState = {
+    quiz: Quiz,
+}
+
+const initialState : InitialState = {
+    quiz: [],
+}
+
+const quizSlice = createSlice({
+    name : "quiz",
+    initialState,
+    reducers: {
+        setQuizData: (state, action) => {
+            console.log("Came here");
+            state.quiz = action.payload
+        },
+    }
+})
+
+export const {setQuizData} = quizSlice.actions
+
+export default quizSlice.reducer;
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import routes from './routes.ts'
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {routes.map(({path, component : Component,}) => <Route path={path} element={<Component />}/>)}
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          {routes.map(({path, component : Component,}) => <Route path={path} element={<Component />}/>)}
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
