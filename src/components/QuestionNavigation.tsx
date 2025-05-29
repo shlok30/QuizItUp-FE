1831c031190aba5804deb277a2ee134bf6894cee
shlok1998 <shlokmehra@rocketmail.com>
2025-05-29 23:37:48 +0530

    feat: Quiz question navigation

--- src/components/QuestionNavigation.tsx
+++ src/components/QuestionNavigation.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 
-function QuestionNavigation({currentNumber, totalNumber} : {currentNumber: number, totalNumber: number}){
+function QuestionNavigation({currentNumber, totalNumber, handleNavigation} : {currentNumber: number, totalNumber: number, handleNavigation: () => void}){
     return(
         <>
-            <span className="text-2xl cursor-pointer">&#8592;</span> 
+            {currentNumber > 1 && <button onClick={handleNavigation}><span className="text-2xl cursor-pointer">&#8592;</span> </button>}
             <p className="lg:text-2xl sm:text-4xl">Question {currentNumber} of {totalNumber}</p>
         </>
     )
