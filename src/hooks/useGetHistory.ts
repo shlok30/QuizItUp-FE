3cac3df5718c020e6ea1b1bde26184c1c972a5cc
shlok1998 <shlokmehra@rocketmail.com>
2025-07-08 14:12:36 +0530

    fix: Stale Token fixed

--- src/hooks/useGetHistory.ts
+++ src/hooks/useGetHistory.ts
@@ -1,9 +1,8 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import { getCacheKey, handleAuthError } from "../utils";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store";
 import useFetch from "./useFetch";
-import { Quizzes } from "../types";
 import endpoints from "../endpoints";
 import { useLocation, useNavigate } from "react-router";
 import { History, setCacheEntry } from "../features/quiz";
@@ -12,19 +11,18 @@ type UseGetHistoryProps = {
     queryParams: URLSearchParams
 }
 
-const options = {headers: {Authorization: `Bearer ${localStorage.getItem("token")}`}};
-
 function useGetHistory({queryParams} : UseGetHistoryProps){
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { search: queryParamsString } = useLocation();
 
+    const token = localStorage.getItem("token");
+    const options = useMemo(() => ({headers: {Authorization: `Bearer ${token}`}}),[token]);
+
     const cacheKey = getCacheKey(queryParams);
     const cacheEntries = useSelector((state: RootState) => state.quiz.history);
     const cachedValue = cacheEntries[cacheKey];
-
-    console.log("Cached Entries",cacheEntries);
 
     const onError = useCallback((response: Response) => handleAuthError(response.status, dispatch, navigate),[dispatch, navigate])
     const {data, isLoading, isError} = useFetch<History>({endpoint: `${endpoints.getAllQuizzes}${queryParamsString}`, options, onError, skip: Boolean(cachedValue)});
